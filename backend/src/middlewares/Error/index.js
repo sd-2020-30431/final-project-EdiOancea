f53ddb4af94a1b2c0c6e13baca06154aad1a11f0
Oancea <EOancea@romania.luxoft.com>
2020-05-01 18:50:33 +0300

    Remove console.log.

--- backend/src/middlewares/Error/index.js
+++ backend/src/middlewares/Error/index.js
@@ -1,6 +1,5 @@
 export default () => (err, _req, res, _next) => {
-    console.log(err, res)
     if (err) {
-        res.status(err.status).json({ error: err.message });
+        res.status(err.status || 500).json({ error: err.message });
     }
 };
