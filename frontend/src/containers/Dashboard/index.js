6e3f13610b94d54116fcc9bc4d10c7c9ac57cf77
Oancea <EOancea@romania.luxoft.com>
2020-05-04 14:38:18 +0300

    Create selector for food entries based on the selected day.

--- frontend/src/containers/Dashboard/index.js
+++ frontend/src/containers/Dashboard/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { getFoodEntries } from 'actions/foodEntry';
 import DashboardComponent from 'components/Dashboard';
+import { selectFoodEntries } from 'selectors/foodEntry';
 
 const Dashboard = () => {
+    const dispatch = useDispatch();
+    const entries = useSelector(selectFoodEntries);
+
+    useEffect(() => {
+        dispatch(getFoodEntries());
+    }, [dispatch]);
+    
     return (
-        <DashboardComponent />
+        <DashboardComponent 
+            entries={entries}
+        />
     );
 };
 
